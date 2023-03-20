c3b7ebcde8e6f36320acd03c19f6b21e230b4ab7
Wellington dos Santos Barbosa <welitonpe@gmail.com>
2023-03-20 01:55:28 -0300

    update prisma schema

--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,7 @@ app.post("/users", async (request, reply) => {
     name: z.string(),
     email: z.string().email(),
   });
+
   const { name, email } = createUserSchema.parse(request.body);
   await prisma.user.create({
     data: {
@@ -25,11 +26,33 @@ app.post("/users", async (request, reply) => {
   return reply.status(201).send();
 });
 
+app.get("/markets", async () => {
+  const market = await prisma.market.findMany();
+  return market;
+});
+
+app.post("/markets", async (request, reply) => {
+  const createMarketSchema = z.object({
+    name: z.string(),
+    local: z.string(),
+  });
+
+  const { name, local } = createMarketSchema.parse(request.body);
+  await prisma.market.create({
+    data: {
+      name,
+      local,
+    },
+  });
+  return reply.status(201).send();
+});
+
 app
   .listen({
     host: "0.0.0.0",
     port: process.env.PORT ? Number(process.env.PORT) : 3333,
   })
-  .then(() => {
+  .then((e) => {
+    console.log(e);
     console.log("HTTP Server Running");
   });
